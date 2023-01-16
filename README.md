031fd788c99423d81c59ae020bfe57fd2ed895ff
cpiernikowski <czarson2115@wp.pl>
2023-01-16 21:18:24 +0100

    1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Program obliczający średni błąd kwadratowy wartości średniej
 ## Dane pobierane są z pliku 'dane', zasady dotyczące formatowania danych:
-- Każdy pomiar oddzielony jest przecinkiem
+- Każdy pomiar oddzielony jest znakiem nowej linii
 - Znak separujący część całkowitą i dziesiętną to kropka
--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include <cmath>
 #include <iostream>
 #include <numeric>
+#include <fstream>
+#include <string>
+#include <vector>
 
 // https://pl.wikipedia.org/wiki/%C5%9Aredni_b%C5%82%C4%85d_%C5%9Bredniej
 
 int main() {
-	static constexpr double dane[] = {
-		#include "dane"
-	};
+	std::ifstream ifs("dane");
+	std::vector<double> dane;
 
-	constexpr auto srednia = std::accumulate(std::begin(dane), std::end(dane), 0.0) / std::size(dane);
-	constexpr auto mianownik = static_cast<double>(std::size(dane) * (std::size(dane) - 1));
-	auto wynik = 0.0;
+	for (std::string line; std::getline(ifs, line);) {
+		dane.push_back(std::stod(line));
+	}
 
-	for (auto i = 0u; i < std::size(dane); ++i) {
+	ifs.close();
+
+	const double srednia = std::accumulate(std::begin(dane), std::end(dane), 0.0) / std::size(dane);
+	const double mianownik = std::size(dane) * (std::size(dane) - 1);
+	double wynik{};
+
+	for (std::size_t i{}; i < std::size(dane); ++i) {
 		wynik += std::pow(dane[i] - srednia, 2.0);
 	}
 
-	wynik /= mianownik;
-	wynik = std::sqrt(wynik);
+	wynik = std::sqrt(wynik / mianownik);
 
 	std::cout << wynik;
 	return 0;
